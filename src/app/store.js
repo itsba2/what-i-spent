f41f3c673dce8a54351e43f193c3196113e32d16
Said Batuhan Bilmez <batuhan.bilmez@tum.de>
2023-12-24 09:22:46 +0100

    feedback redux implementation

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { setupListeners } from "@reduxjs/toolkit/query"
-import { expenseApi } from "./services/expenseApi"
-import { earningApi } from "./services/earningApi"
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { expenseApi } from "./services/expenseApi";
+import { earningApi } from "./services/earningApi";
+import feedbackReducer from "./feedbackSlice";
 
 export const store = configureStore({
-    reducer: {
-        // TODO: loading/feedback slice
-        [expenseApi.reducerPath]: expenseApi.reducer,
-        [earningApi.reducerPath]: earningApi.reducer,
-    },
-    middleware: (dMw) =>
-        dMw().concat(expenseApi.middleware).concat(earningApi.middleware),
-})
+  reducer: {
+    feedback: feedbackReducer,
+    [expenseApi.reducerPath]: expenseApi.reducer,
+    [earningApi.reducerPath]: earningApi.reducer,
+  },
+  middleware: (dMw) =>
+    dMw().concat(expenseApi.middleware).concat(earningApi.middleware),
+  devTools: import.meta.env.VITE_ENV === "dev",
+});
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
